74f12e8444507423cb68bde5a5708049f1c0623d
Wyatt Johnson <accounts+github@wyattjoh.ca>
2022-07-19 09:30:02 -0600

    fix: added empty `<head />`

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head />
       <body className="bg-red-400">{children}</body>
     </html>
   );
